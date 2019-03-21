d24d9f80e08c5f22d0ebbab2a90d0b42963665ff
tanjing1019 <37397120+tanjing1019@users.noreply.github.com>
2019-03-21 10:56:48 +0800

    提交代码
    提交代码

---
+++ soap_server.php
@@ -0,0 +1,15 @@
+<?php   
+
+$soap = new SoapClient(null, array('location'=>'http://localhost/php_webservice/server.php','uri' =>'http://soap/'));     
+  
+echo $soap->show();   
+//得到：'the data you request!'   
+  
+//echo $soap->getUserInfo('sss');
+
+
+//https://www.cnblogs.com/AloneSword/p/3501541.html
+//https://blog.csdn.net/s371795639/article/details/80008486
+//https://www.cnblogs.com/lauhp/p/7999675.html
+
+?>
