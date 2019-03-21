d489b4308350859ba9c30d61febdb91e0f6feb49
tanjing1019 <37397120+tanjing1019@users.noreply.github.com>
2019-03-21 11:15:51 +0800

    重新命名
    Signed-off-by: tanjing1019 <37397120+tanjing1019@users.noreply.github.com>

--- soap_server.php
+++ soap_server.php
@@ -1,15 +1,30 @@
 <?php   
+class test   
+{   
+    function show()   
+    {   
+        return 'the data you request!';   
+    }   
+}   
+function getUserInfo($name)   
+{   
+    return 'fbbin';   
+}   
+//实例化的参数手册上面有，这个是没有使用wsdl的，所以第一个参数为null，如果有使用wsdl，那么第一个参数就是这个wsdl文件的地址。   
+$server = new SoapServer(null, array('uri' => "http://soap/",'location' => "http://127.0.0.1/php_webservice/server.php"));  
+$server->setClass('test');   
+//$server->addFunction('getUserInfo');   
+$server->handle();   
 
-$soap = new SoapClient(null, array('location'=>'http://localhost/php_webservice/server.php','uri' =>'http://soap/'));     
-  
-echo $soap->show();   
+
+
 //得到：'the data you request!'   
   
 //echo $soap->getUserInfo('sss');
 
+ 
 
-//https://www.cnblogs.com/AloneSword/p/3501541.html 
+//https://www.cnblogs.com/AloneSword/p/3501541.html
 //https://blog.csdn.net/s371795639/article/details/80008486
 //https://www.cnblogs.com/lauhp/p/7999675.html
-
 ?>
